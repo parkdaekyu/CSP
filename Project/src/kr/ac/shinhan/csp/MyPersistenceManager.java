51594e5e64ccea08a225b9850600c1f51b43dc74
Park <Park@Park-PC>
2015-04-12 23:48:50 +0900

    project

---
+++ Project/src/kr/ac/shinhan/csp/MyPersistenceManager.java
@@ -0,0 +1,13 @@
+package kr.ac.shinhan.csp;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+
+public class MyPersistenceManager {
+	public static PersistenceManager pm = JDOHelper.getPersistenceManagerFactory("transactions-optional").getPersistenceManager();
+	public static PersistenceManager getManager() 
+ 	{
+ 		return pm;
+ 	}
+
+}
